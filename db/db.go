aeb5cee42d4f95c638ecd4d18e4c1040bcffcaaa
tareq.abedrabbo@opencredo.com <tareq.abedrabbo@opencredo.com>
2013-06-18 01:16:55 +0100

    Add documentation

--- db/db.go
+++ db/db.go
@@ -1,5 +1,6 @@
 package db
 
+// The Db interface defines all the public methods provided by the database.
 type Db interface {
 	Get(key string) (*Record, error)
 	Set(key string, value []byte) (*Record, error)
